fix opacity distance computation

glimmer/__init__.py
@@ -5,6 +5,10 @@ import numpy as np
 
 from glimmer.chu import StrattonChu, EH_Gaussian
 
+import pyvista as pv
+
+pv.global_theme.colorbar_orientation = "vertical"
+
 
 def unpack(f):
 
@@ -174,7 +178,7 @@ class Grid(StructuredGrid):
         self["|<S>|"], self["|<S>| (dB)"] = logclip(S0)
 
         self.set_active_vectors("Sr")
-        self.set_active_scalars("|<S>|")
+        self.set_active_scalars("|E|^2")
 
     def get_fields(self):
 
@@ -311,7 +315,12 @@ class Problem:
         for i, (obj, actor) in enumerate(self.actors):
             if obj.dimensionality == 3:
                 self.plotter.remove_actor(actor)
-                new_actor = self.plotter.add_volume(obj, clim=self.clim, cmap=self.cmap)
+                new_actor = self.plotter.add_volume(
+                    obj,
+                    clim=self.clim,
+                    cmap=self.cmap,
+                    opacity_unit_distance=obj.length / np.linalg.norm(obj.dimensions),
+                )
                 self.actors[i] = (obj, new_actor)
 
         self.plotter.render()
@@ -320,17 +329,24 @@ class Problem:
 
         self.plotter = Plotter()
 
-        objects = [self.source, *self.optics, *self.probes]
+        objects = [*self.probes, *self.optics, self.source]
 
         scalars = np.concat([np.ravel(obj.active_scalars) for obj in objects])
 
         self.clim = (np.nanmin(scalars), np.nanmax(scalars))
 
         def add_object(obj):
-            try:
-                return self.plotter.add_volume(obj, clim=self.clim, cmap=self.cmap)
-            except:
-                return self.plotter.add_mesh(obj, clim=self.clim, cmap=self.cmap)
+            if obj.dimensionality == 3:
+                actor = self.plotter.add_volume(
+                    obj,
+                    clim=self.clim,
+                    cmap=self.cmap,
+                    opacity_unit_distance=obj.length / np.linalg.norm(obj.dimensions),
+                )
+            else:
+                actor = self.plotter.add_mesh(obj, clim=self.clim, cmap=self.cmap)
+
+            return actor
 
         self.actors = [(obj, add_object(obj)) for obj in objects]
 

glimmer/templates/gt.py
@@ -3,10 +3,10 @@ from glimmer import Gaussian, Mirror, Volume, Problem
 import numpy as np
 
 
-from glimmer.mom import poly2grid
+from glimmer.tools import poly2grid
 from scipy.constants import milli
 
-src = Gaussian(w0=10 * milli, lam=3 * milli, num_lam=3, num_waist=2)
+src = Gaussian(w0=10 * milli, lam=3 * milli, num_lam=3, num_waist=2.0)
 src.rotate_z(5)
 src.rotate_y(3)
 
@@ -25,12 +25,15 @@ m1.translate([0, 0, s])
 
 m2 = Mirror(**options)
 m2.rotate_x(-45)
+m2.rotate_y(-5)
 m2.translate([0, 2 * s, s])
 
 pec = src + m1 + m2
 
 vol = poly2grid(pec, d=2 * milli, l=10 * milli)
+vol.chunks = 32
 yz = poly2grid(pec, d=milli / 2, x=0, l=10 * milli)
+yz.chunks = 32
 
 
 def preview():
@@ -44,8 +47,3 @@ def preview():
 if __name__ == "__main__":
 
     preview()
-
-    # yz = Volume(d=1, ylim=(-L / 2, 2 * s + L / 2), zlim=(0, s + L / 2))
-    # vol = Volume(
-    #     d=5, xlim=(-L / 2, L / 2), ylim=(-L / 2, 2 * s + L / 2), zlim=(0, s + L / 2)
-    # )