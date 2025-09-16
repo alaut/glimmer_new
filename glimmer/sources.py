e07aed5f7e42e4b158aaa88fc3bf8f1eea8bb736
Alex Laut <14655890+alaut@users.noreply.github.com>
2025-09-16 11:47:38 -0700

    cleanup TE mode definition

--- glimmer/sources.py
+++ glimmer/sources.py
@@ -11,46 +11,46 @@ eta = np.sqrt(mu_0 / epsilon_0)
 class TransverseElectric(pv.StructuredGrid):
 
     lam: float
+
     m: int
     n: int
 
     a: float = None
     omf: float = 1.25
 
-    nr: int = 8 * 2
-    nt: int = 8 * 6 * 2
+    num_lam: float = 12
+
+    rmin: float = 0
 
     def __post_init__(self):
 
-        self.k0 = 2 * np.pi / self.lam
+        k0 = 2 * np.pi / self.lam
 
         nu = jnp_zeros(self.m, self.n)[-1]
 
         if self.a is None:
-            self.a = self.omf * nu / self.k0
+            self.a = self.omf * nu / k0
         else:
-            self.omf = self.a / (nu / self.k0)
+            self.omf = self.a / (nu / k0)
 
-        kr = nu / self.a
-        kz = np.sqrt(self.k0**2 - kr**2)
+        dl = self.lam / self.num_lam
+        nr = int(np.max([self.n * self.num_lam, (self.a - self.rmin) / dl]))
+        nt = int(np.max([self.m * self.num_lam, 2 * np.pi * self.a / dl]))
 
-        # caustic radius
-        ac = self.a * self.m / nu
-
-        R, T, Z = np.broadcast_arrays(
-            np.linspace(self.a, ac / 1.5, self.nr, endpoint=False)[..., None],
-            np.linspace(0, 2 * np.pi, self.nt),
-            0,
+        R, T, Z = np.meshgrid(
+            np.linspace(self.rmin, self.a, nr), np.linspace(0, 2 * np.pi, nt), 0
         )
-        X = R * np.cos(T)
-        Y = R * np.sin(T)
 
-        super().__init__(X, Y, Z)
+        super().__init__(R * np.cos(T), R * np.sin(T), Z)
 
-        x, y, z = np.moveaxis(self.points, -1, 0)
+        x = self.points[..., 0]
+        y = self.points[..., 1]
 
         r = np.sqrt(x**2 + y**2)
         t = np.atan2(y, x)
+
+        kr = nu / self.a
+        kz = np.sqrt(k0**2 - kr**2)
 
         J = jv(self.m, kr * r)
         Jp = jvp(self.m, kr * r)
@@ -60,16 +60,18 @@ class TransverseElectric(pv.StructuredGrid):
 
         Hz = J * C
         Hr = -1j * kz / kr * Jp * C
-        Ht = 1j * self.m * kz / r / kr**2 * J * S
+        with np.errstate(invalid="ignore", divide="ignore"):
+            Ht = 1j * self.m * kz / r / kr**2 * J * S
+            Ht[r == 0] = 0
 
-        Z = self.k0 / kz * eta
+        Z = k0 / kz * eta
 
         Er = Z * Ht
         Et = -Z * Hr
 
         Ex = np.cos(t) * Er - np.sin(t) * Et
         Ey = np.sin(t) * Er + np.cos(t) * Et
-        Ez = np.zeros_like(z)
+        Ez = np.zeros_like(r)
 
         Hx = np.cos(t) * Hr - np.sin(t) * Ht
         Hy = np.sin(t) * Hr + np.cos(t) * Ht
@@ -77,10 +79,20 @@ class TransverseElectric(pv.StructuredGrid):
         E = np.stack([Ex, Ey, Ez], axis=-1)
         H = np.stack([Hx, Hy, Hz], axis=-1)
 
-        self["Er"] = E.real
-        self["Ei"] = E.imag
-        self["Hr"] = H.real
-        self["Hi"] = H.imag
+        self["Er"] = np.real(E)
+        self["Ei"] = np.imag(E)
 
-        self["|E|^2"] = np.linalg.norm(E, axis=-1) ** 2
-        self["|H|^2"] = np.linalg.norm(H, axis=-1) ** 2
+        self["Hr"] = np.real(H)
+        self["Hi"] = np.imag(H)
+
+        self["|E|"] = np.abs(E)
+        self["|H|"] = np.abs(H)
+
+        self.set_active_scalars("|E|")
+        self.set_active_vectors("|E|")
+
+
+if __name__ == "__main__":
+
+    te = TransverseElectric(lam=300 / 110, m=22, n=6, omf=1.06)
+    te.plot()
