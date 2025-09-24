3990155e507578e108bb8a2681faa253e59c90eb
Alex Laut <14655890+alaut@users.noreply.github.com>
2025-09-24 11:54:43 -0700

    update template

--- glimmer/templates/gaussian_telescope.py
+++ glimmer/templates/gaussian_telescope.py
@@ -4,14 +4,15 @@ import numpy as np
 
 from scipy.constants import milli
 
+# milli = 1
 lam = 3 * milli
 w0 = 10 * milli
 
 num_waist = 2
 num_lam = 3
 
-src = Gaussian(w0=w0, lam=lam, num_lam=num_lam, num_waist=2)
-src.rotate_x(3, inplace=True, transform_all_input_vectors=True)
+src = Gaussian(w0=w0, lam=lam, num_lam=num_lam, num_waist=num_waist)
+# src.rotate_x(3, inplace=True, transform_all_input_vectors=True)
 
 zR = np.pi * w0**2 / lam
 s = zR / 2
@@ -32,15 +33,26 @@ m2.translate([0, 2 * s, s], inplace=True)
 add_field(m1, key="E")
 add_field(m2, key="E")
 
-# probe planes
+optics = [
+    m1,
+    m2,
+]
+
 pec = src + m1 + m2
 
-vol = Grid(ds=pec, d=2 * milli, scale=1.2)
+vol = Grid(ds=pec, d=1 * milli, scale=1.2)
 
 yz = Grid(ds=pec, xlim=0, d=1 / 4 * milli, scale=1.1)
 xy = Grid(xlim=(-L / 2, L / 2), ylim=(-L / 2, L / 2), d=1 / 4 * milli)
-xz = Grid(xlim=(-L / 2, L / 2), zlim=(-L / 2, L / 2), d=1 / 4 * milli)
 
+xz = xy.rotate_x(90).translate([0, s, s])
 xy1 = xy.translate([0, 0, 5 * milli])
 xy2 = xy.translate([0, 2 * s, 5 * milli])
-xz = xz.translate([0, s, s])
+
+probes = [
+    # vol,
+    yz,
+    xy1,
+    xz,
+    xy2,
+]
