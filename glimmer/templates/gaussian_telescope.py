09f34b0dc14d3fb64e4b987a018794d76b3c6c0b
Alex Laut <14655890+alaut@users.noreply.github.com>
2025-09-25 09:56:58 -0700

    update gt example

--- glimmer/templates/gaussian_telescope.py
+++ glimmer/templates/gaussian_telescope.py
@@ -1,18 +1,20 @@
-from glimmer import Gaussian, Mirror, Grid, add_field
+import pyvista as pv
+from glimmer import Mirror, Grid, add_field
 import numpy as np
-
-
+from glimmer.tools import remesh
+from glimmer.sources import GaussianBeam
 from scipy.constants import milli
 
 # milli = 1
+
 lam = 3 * milli
 w0 = 10 * milli
 
 num_waist = 2
-num_lam = 3
+num_lam = 2
 
-src = Gaussian(w0=w0, lam=lam, num_lam=num_lam, num_waist=num_waist)
-# src.rotate_x(3, inplace=True, transform_all_input_vectors=True)
+src = GaussianBeam(w0=w0, lam=lam, num_lam=num_lam, num_waist=num_waist)
+src.rotate_x(3, inplace=True, transform_all_input_vectors=True)
 
 zR = np.pi * w0**2 / lam
 s = zR / 2
@@ -20,14 +22,22 @@ c = np.cos(np.pi / 4)
 
 L = num_waist * w0 * (1 + (s / zR) ** 2) ** 0.5
 
-options = {"L": (L, L / c), "dL": lam / num_lam, "f": (s * c, s / c)}
+options = {
+    "L": (L, L / c),
+    "dL": lam / num_lam,
+    "f": (s * c, s / c),
+    # "f": np.inf,
+}
 
-m1 = Mirror(**options)
-m1.rotate_x(45, inplace=True)
+m = Mirror(**options)
+
+# m = remesh(m.extract_surface().triangulate(), milli)
+
+m1 = m.rotate_x(45)
 m1.translate([0, 0, s], inplace=True)
 
-m2 = Mirror(**options)
-m2.rotate_x(-45, inplace=True)
+
+m2 = m.rotate_x(-77)
 m2.translate([0, 2 * s, s], inplace=True)
 
 add_field(m1, key="E")
@@ -38,9 +48,12 @@ optics = [
     m2,
 ]
 
-pec = src + m1 + m2
 
-vol = Grid(ds=pec, d=1 * milli, scale=1.2)
+pec = pv.merge([src, *optics])
+
+# pec = remesh(pec.extract_surface().triangulate(), milli)
+
+vol = Grid(ds=pec, d=1.5 * milli, scale=1.2)
 
 yz = Grid(ds=pec, xlim=0, d=1 / 4 * milli, scale=1.1)
 xy = Grid(xlim=(-L / 2, L / 2), ylim=(-L / 2, L / 2), d=1 / 4 * milli)
