92d5dfaa4563127f7a54b48cb9dd548b0fc3c1a5
Alex Laut <14655890+alaut@users.noreply.github.com>
2025-09-26 10:58:26 -0700

    update example to solve different solving modes

--- tests/gt_mom.py
+++ tests/gt_mom.py
@@ -1,18 +1,47 @@
 from glimmer.templates import gaussian_telescope as gt
 from glimmer.mom import Solver
 from glimmer.tools import integrate_power
+from glimmer import Timer
 
-solver = Solver(ds=gt.pec, lam=gt.lam, probes=gt.probes)
-
-# solver.plot().show()
-solver.solve()
+prefix = "./temp/gt/mom"
 
 
-for obj in solver.probes:
-    integrate_power(obj)
+def main(mode="cupy-sparse"):
 
-solver.save("./temp/gt/mom")
-plotter = solver.plot()
+    solver = Solver(
+        ds=gt.pec,
+        lam=gt.lam,
+        probes=gt.probes,
+        mode=mode,
+    )
+
+    # solver.plot().show()
+    solver.solve()
+
+    print(time.elapsed)
+
+    # for obj in solver.probes:
+    #     integrate_power(obj)
+
+    # solver.save(prefix)
+    # plotter = solver.plot()
+
+    # plotter.show()
 
 
-plotter.show()
+import pandas as pd
+
+if __name__ == "__main__":
+
+    modes = ["cupy-sparse", "cupy", "numpy-sparse", "tensorflow", "pytorch"]
+    df = pd.DataFrame({"time": 0}, index=modes)
+
+    for mode in modes:
+
+        with Timer(mode) as time:
+            main(mode)
+
+        df["time"][mode] = time.elapsed.total_seconds()
+    df = df.sort_values(by="time")
+    df.to_excel(f"{prefix}runtime.xlsx")
+    print(df)
